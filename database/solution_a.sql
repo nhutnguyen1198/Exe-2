ca465af1d2405f070930864738d3068e6aee7e44
nhutnguyen1198 <nm11nhut@gmail.com>
2025-03-20 23:17:43 +0700

    update Exe-2

---
+++ database/solution_a.sql
@@ -0,0 +1,48 @@
+﻿--câu 1 lấy danh sách người dùng theo thứ tự tên theo Alphabet(A->Z)
+SELECT *
+FROM Users
+ORDER BY user_name ASC;
+--câu 2 lấy ra 07 người dùng theo thứ tự tên Alphabet(A->Z)
+SELECT *
+FROM Users
+ORDER BY user_name ASC LIMIT 7;
+--câu 3 lây ra danh sách người dùng theo thứ tự tên theo Alphebet(A->Z), trong đó tên người dùng có chữ a
+SELECT *
+FROM Users
+WHERE user_name like '%a%'
+ORDER BY user_name ASC ;
+--câu 4 lấy ra danh sách người dùng trong đó tên người dùng bắt đầu bằng chứ m
+SELECT *
+FROM Users
+WHERE user_name like 'm%';
+--câu 5 lấy ra danh sách người dùng trong đó tên người dùng kết thúc bằng chữ i
+SELECT *
+FROM Users
+WHERE user_name like '%i';
+--câu 6 lấy ra danh sách người dùng trong đó email người dùng là Gmail
+SELECT *
+FROM Users
+WHERE user_email like '%@gmail.com';
+--câu 7 lấy danh sách người dùng trong đó email người dùng là Gmail, tên người dùng bắt đầu bằng chữ m
+SELECT *
+FROM Users
+WHERE user_email like '%@gmail.com' && user_name LIKE 'm%';
+--câu 8 lấy danh sách người dùng trong đó email người dùng là Gmail, tên người dùng có chữ i và tên người dùng có chiều dài lớn hơn 5
+SELECT *
+FROM Users
+WHERE user_email LIKE '%@gmail.com'
+  AND user_name LIKE '%i%' 
+  AND CHAR_LENGTH(user_name) > 5;
+--câu 9 lấy ra danh sách người dùng trong đó tên người dùng có chữ a, chiều dài từ 5 đến 9,dùng dịch vụ Gmail trong tên email có chữ I
+SELECT * FROM users 
+WHERE user_name LIKE '%a%' 
+AND CHAR_LENGTH(user_name) BETWEEN 5 AND 9
+AND user_email LIKE '%@gmail.com'
+AND SUBSTRING_INDEX(user_email, '@', 1) LIKE '%I%';
+--câu 10 lấy ra danh sách người dùng trong đó tên người dùng có chữ a, chiều dài từ 5 đến 9 hoặc tên người dùng có chữ i, chiều dài nhỏ hơn 9 hoặc email dùng dịch vụ Gmail, trong tên email có chữ i
+SELECT * FROM users 
+WHERE (user_name LIKE '%a%' 
+AND CHAR_LENGTH(user_name) BETWEEN 5 AND 9)
+OR (user_name LIKE '%i%' AND CHAR_LENGTH(user_name) < 9)
+OR (user_email LIKE '%@gmail.com'
+AND SUBSTRING_INDEX(user_email, '@', 1) LIKE '%I%');
---
+++ database/solution_b.sql
@@ -0,0 +1,43 @@
+﻿--1 Liệt kê các hóa đơn của khách hàng, thông tin hiển thị gồm: mã user, tên user, mã hóa đơn
+SELECT u.user_id, u.user_name, o.order_id  
+FROM orders o, users u
+WHERE o.user_id = u.user_id
+--2 Liệt kê số lượng các hóa đơn của khách hàng: mã user, tên user, số đơn hàng
+SELECT u.user_id, u.user_name, COUNT(o.order_id) AS total_orders  
+FROM orders o, users u  
+WHERE o.user_id = u.user_id  
+GROUP BY u.user_id, u.user_name;
+--3 Liệt kê thông tin hóa đơn: mã đơn hàng, số sản phẩm
+SELECT od.order_id, COUNT(od.product_id) AS total_products  
+FROM order_details od  
+GROUP BY od.order_id
+--4 Liệt kê thông tin mua hàng của người dùng: mã user, tên user, mã đơn hàng, tên sản phẩm. Lưu ý: gôm nhóm theo đơn hàng, tránh hiển thị xen kẻ các đơn hàng với nhau
+SELECT u.user_id, u.user_name, o.order_id, GROUP_CONCAT(p.product_name SEPARATOR ', ') AS products  
+FROM users u  
+JOIN orders o ON u.user_id = o.user_id  
+JOIN order_details od ON o.order_id = od.order_id  
+JOIN products p ON od.product_id = p.product_id  
+GROUP BY u.user_id, u.user_name, o.order_id  
+ORDER BY o.order_id;
+--5 Liệt kê 7 người dùng có số lượng đơn hàng nhiều nhất, thông tin hiển thị gồm: mã user, tên user, số lượng đơn hàng
+SELECT u.user_id, u.user_name, COUNT(o.order_id) AS total_orders  
+FROM users u  
+JOIN orders o ON u.user_id = o.user_id  
+GROUP BY u.user_id, u.user_name  
+ORDER BY total_orders DESC  
+LIMIT 7
+--6 Liệt kê 7 người dùng mua sản phẩm có tên: Samsung hoặc Apple trong tên sản phẩm, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tên sản phẩm
+SELECT DISTINCT u.user_id, u.user_name, o.order_id, p.product_name  
+FROM users u  
+JOIN orders o ON u.user_id = o.user_id  
+JOIN order_details od ON o.order_id = od.order_id  
+JOIN products p ON od.product_id = p.product_id  
+WHERE p.product_name LIKE '%Samsung%' OR p.product_name LIKE '%Apple%'  
+LIMIT 7;
+--7 Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền
+
+--8 Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền. Mỗi user chỉ chọn ra 1 đơn hàng có giá tiền lớn nhất.
+
+--9 Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền, số sản phẩm. Mỗi user chỉ chọn ra 1 đơn hàng có giá tiền nhỏ nhất.
+
+--10 Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền, số sản phẩm. Mỗi user chỉ chọn ra 1 đơn hàng có số sản phẩm là nhiều nhất.
