0b39f5a8ea54cd936aabe6fad745cb9d432eccde
nhutnguyen1198 <nm11nhut@gmail.com>
2025-03-27 22:39:36 +0700

    update solution_b.sql

--- database/solution_b.sql
+++ database/solution_b.sql
@@ -35,9 +35,59 @@ JOIN products p ON od.product_id = p.product_id
 WHERE p.product_name LIKE '%Samsung%' OR p.product_name LIKE '%Apple%'  
 LIMIT 7;
 --7 Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền
-
+SELECT u.user_id, u.user_name, o.order_id AS, o.total_amount
+FROM 
+    users u
+JOIN 
+    orders o ON u.user_id = o.user_id;
 --8 Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền. Mỗi user chỉ chọn ra 1 đơn hàng có giá tiền lớn nhất.
-
+SELECT 
+    u.user_id, u.user_name, o.order_id, o.total_amount
+FROM 
+    users u
+JOIN 
+    orders o ON u.user_id = o.user_id
+WHERE 
+    o.total_amount = (
+        SELECT MAX(o2.total_amount)
+        FROM orders o2
+        WHERE o2.user_id = u.user_id
+    );
 --9 Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền, số sản phẩm. Mỗi user chỉ chọn ra 1 đơn hàng có giá tiền nhỏ nhất.
-
+SELECT 
+	u.user_id, u.user_name, o.order_id AS, o.total_amount AS, COUNT(od.product_id) AS 'So San Pham'
+FROM 
+    users u
+JOIN 
+    orders o ON u.user_id = o.user_id
+JOIN 
+    order_details od ON o.order_id = od.order_id
+WHERE 
+    o.total_amount = (
+        SELECT MIN(o2.total_amount)
+        FROM orders o2
+        WHERE o2.user_id = u.user_id
+    )
+GROUP BY 
+    u.user_id, u.user_name, o.order_id, o.total_amount;
 --10 Liệt kê danh sách mua hàng của user bao gồm giá tiền của mỗi đơn hàng, thông tin hiển thị gồm: mã user, tên user, mã đơn hàng, tổng tiền, số sản phẩm. Mỗi user chỉ chọn ra 1 đơn hàng có số sản phẩm là nhiều nhất.
+SELECT 
+	u.user_id, u.user_name, o.order_id, o.total_amount, COUNT(od.product_id)
+FROM 
+    users u
+JOIN 
+    orders o ON u.user_id = o.user_id
+JOIN 
+    order_details od ON o.order_id = od.order_id
+GROUP BY 
+    u.user_id, u.user_name, o.order_id, o.total_amount
+HAVING 
+    COUNT(od.product_id) = (
+        SELECT COUNT(od2.product_id)
+        FROM order_details od2
+        JOIN orders o2 ON od2.order_id = o2.order_id
+        WHERE o2.user_id = u.user_id
+        GROUP BY o2.order_id
+        ORDER BY COUNT(od2.product_id) DESC
+        LIMIT 1
+    );
